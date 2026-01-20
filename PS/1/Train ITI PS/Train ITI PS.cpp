be2922f138afd0ad255eb9cd020f2d2e8fe6c484
Mostafa Shanab <2021170523@cis.asu.edu.eg>
2026-01-20 14:52:47 +0200

    finished day1 html5&css3

--- PS/1/Train ITI PS/Train ITI PS.cpp
+++ PS/1/Train ITI PS/Train ITI PS.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -6,6 +7,19 @@ int main()
 {
     int n, m;
     cin >> n >> m;
-    
+    int res = 0;
+
+    for (int a = 0; a <= sqrt(max(n, m)); a++)
+    {
+        int b = n - a * a;
+        if (b * b + a == m)
+        {
+            cout << "a: " << a << " b: " << b << endl;
+            res++;
+        }
+    }
+
+    cout << res << endl;
+
     return 0;
 }

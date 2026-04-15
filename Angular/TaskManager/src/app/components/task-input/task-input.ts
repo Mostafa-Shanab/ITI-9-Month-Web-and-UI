b3543e9bd75d4371bc08f0609ecd29caf50efdf3
Mostafa Shanab <2021170523@cis.asu.edu.eg>
2026-04-16 01:46:49 +0200

    updated repos for day1 & day2 and finished point 2 in task day2

--- Angular/TaskManager/src/app/components/task-input/task-input.ts
+++ Angular/TaskManager/src/app/components/task-input/task-input.ts
@@ -1,8 +1,55 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Task {
+  title: string;
+  description: string;
+  priority: 'Low' | 'Medium' | 'High';
+  dueDate: string;
+  category: 'Work' | 'Personal' | 'Study';
+  tags: string;
+}
 
 @Component({
   selector: 'app-task-input',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './task-input.html',
   styleUrl: './task-input.css',
 })
-export class TaskInputComponent {}
+export class TaskInputComponent {
+  title: string = '';
+  description: string = '';
+  priority: Task['priority'] = 'Low';
+  dueDate: string = '';
+  category: Task['category'] = 'Work';
+  tags: string = '';
+
+  tasks: Task[] = [];
+
+  addTask(): void {
+    const newTask: Task = {
+      title: this.title,
+      description: this.description,
+      priority: this.priority,
+      dueDate: this.dueDate,
+      category: this.category,
+      tags: this.tags,
+    };
+
+    this.tasks.push(newTask);
+
+    console.log(this.tasks);
+
+    this.resetForm();
+  }
+
+  resetForm(): void {
+    this.title = '';
+    this.description = '';
+    this.priority = 'Low';
+    this.dueDate = '';
+    this.category = 'Work';
+    this.tags = '';
+  }
+}
